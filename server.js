273899deab45308ee3fcade3e321a2fa94af933b
duy.phan <phanduyt291@gmail.com>
2017-05-25 23:40:54 +0700

    Added http server module

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 var http = require('http');
-
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
+var url = require('url');
+/*
+var server = http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.end('Hello Http');
 });
-server.listen(8081);
+*/
+function start(route) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        
+        route(pathname);
+        
+        response.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+        response.write("Hello World!!");
+        response.end();
+    }
+    http.createServer(onRequest).listen(8081);
+    console.log("Server has started.")
+}
+
+exports.start = start;
+exports.info = function() {console.log("Http server! port 8081")};
+//server.listen(8081);
