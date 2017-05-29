5cb3896c033f35bec488bb1582b077d6d5496058
duy.phan <phanduyt291@gmail.com>
2017-05-29 23:20:39 +0700

    Supported file upload (#3)
    Signed-off-by: duy.phan <phanduyt291@gmail.com>

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ var server = http.createServer(function (req, res) {
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
+/*
+        request.setEncoding("utf8");
+        
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" + postDataChunk + "'.");
+        });
+        
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
+*/
 
 /*
         response.writeHead(200, {
@@ -19,9 +32,10 @@ function start(route, handle) {
         });
 */
 
-        var content = route(handle, pathname, response);
+//        var content = route(handle, pathname, response);
 //        response.write(content);
 //        response.end();
+        route(handle, pathname, response, request);
     }
     http.createServer(onRequest).listen(8081);
     console.log("Server has started.");
